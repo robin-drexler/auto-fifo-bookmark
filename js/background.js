265aae1060d80177560c4285d74e980ad441d412
Robin Drexler <drexler.robin@gmail.com>
2014-12-04 15:25:01 +0100

    proof of concept. basic functionality with hardcoded folder id

---
+++ js/background.js
@@ -0,0 +1,57 @@
+(function () {
+    const BOOKMARKS_TO_KEEP = 10;
+
+    var fifoFolderId = "345";
+
+    function removeSuperfluousNodesFromFifoFolder() {
+        chrome.bookmarks.getSubTree(fifoFolderId, function (fifoFolderNode) {
+            fifoFolderNode = fifoFolderNode[0]; //not sure why it is an array?
+
+            var toDeleteNodes;
+
+            if (!fifoFolderNode.children) {
+                return;
+            }
+
+            toDeleteNodes = fifoFolderNode.children.slice(BOOKMARKS_TO_KEEP);
+
+            toDeleteNodes.forEach(function (toDeleteNode) {
+                chrome.bookmarks.removeTree(toDeleteNode.id);
+            });
+        });
+    }
+
+    function moveBookmarkToTop(id, cb) {
+        chrome.bookmarks.move(id, {index: 0}, function (_) {
+            cb();
+        });
+    }
+
+    function createdHandler(id, changedNode) {
+        if (changedNode.parentId !== fifoFolderId) {
+            // we only care about changes in our fifo folder
+            return;
+        }
+
+        moveBookmarkToTop(changedNode.id, removeSuperfluousNodesFromFifoFolder);
+    }
+
+    function movedHandler(id, changedNode) {
+
+        if (changedNode.parentId !== fifoFolderId) {
+            // we only care about changes in our fifo folder
+            return;
+        }
+
+        if (changedNode.oldParentId === fifoFolderId) {
+            // it was moved inside the folder, to prevent infinite recursion: STAAAP
+            return;
+        }
+
+        moveBookmarkToTop(id, removeSuperfluousNodesFromFifoFolder);
+    }
+
+    chrome.bookmarks.onCreated.addListener(createdHandler);
+    chrome.bookmarks.onMoved.addListener(movedHandler);
+
+})();
