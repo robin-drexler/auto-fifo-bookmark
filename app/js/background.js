cad0bff426cf31257a03dff511a974d9f34429e7
Robin Drexler <drexler.robin@gmail.com>
2015-03-05 21:42:42 +0100

    indentation

--- app/js/background.js
+++ app/js/background.js
@@ -1,83 +1,83 @@
-
 function background(chromeBookmarkService, fifoFolderId) {
 
-    if (chromeBookmarkService === undefined) {
+  if (chromeBookmarkService === undefined) {
+    return;
+  }
+
+  const BOOKMARKS_TO_KEEP = 10;
+
+
+  function removeSuperfluousNodesFromFifoFolder() {
+    chrome.bookmarks.getSubTree(fifoFolderId, function (fifoFolderNode) {
+      fifoFolderNode = fifoFolderNode[0]; //not sure why it is an array?
+
+      var toDeleteNodes;
+
+      if (!fifoFolderNode.children) {
         return;
+      }
+
+      toDeleteNodes = fifoFolderNode.children.slice(BOOKMARKS_TO_KEEP);
+
+      toDeleteNodes.forEach(function (toDeleteNode) {
+        chrome.bookmarks.removeTree(toDeleteNode.id);
+      });
+    });
+  }
+
+  function moveBookmarkToTop(id, cb) {
+    chrome.bookmarks.move(id, {index: 0}, function (_) {
+      cb();
+    });
+  }
+
+  function createdHandler(id, changedNode) {
+    if (changedNode.parentId !== fifoFolderId) {
+      // we only care about changes in our fifo folder
+      return;
     }
 
-    const BOOKMARKS_TO_KEEP = 10;
+    moveBookmarkToTop(changedNode.id, removeSuperfluousNodesFromFifoFolder);
+  }
 
+  function movedHandler(id, changedNode) {
 
-    function removeSuperfluousNodesFromFifoFolder() {
-        chrome.bookmarks.getSubTree(fifoFolderId, function (fifoFolderNode) {
-            fifoFolderNode = fifoFolderNode[0]; //not sure why it is an array?
-
-            var toDeleteNodes;
-
-            if (!fifoFolderNode.children) {
-                return;
-            }
-
-            toDeleteNodes = fifoFolderNode.children.slice(BOOKMARKS_TO_KEEP);
-
-            toDeleteNodes.forEach(function (toDeleteNode) {
-                chrome.bookmarks.removeTree(toDeleteNode.id);
-            });
-        });
+    if (changedNode.parentId !== fifoFolderId) {
+      // we only care about changes in our fifo folder
+      return;
     }
 
-    function moveBookmarkToTop(id, cb) {
-        chrome.bookmarks.move(id, {index: 0}, function (_) {
-            cb();
-        });
+    if (changedNode.oldParentId === fifoFolderId) {
+      // it was moved inside the folder, to prevent infinite recursion: STAAAP
+      return;
     }
 
-    function createdHandler(id, changedNode) {
-        if (changedNode.parentId !== fifoFolderId) {
-            // we only care about changes in our fifo folder
-            return;
-        }
-
-        moveBookmarkToTop(changedNode.id, removeSuperfluousNodesFromFifoFolder);
-    }
-
-    function movedHandler(id, changedNode) {
-
-        if (changedNode.parentId !== fifoFolderId) {
-            // we only care about changes in our fifo folder
-            return;
-        }
-
-        if (changedNode.oldParentId === fifoFolderId) {
-            // it was moved inside the folder, to prevent infinite recursion: STAAAP
-            return;
-        }
-
-        moveBookmarkToTop(id, removeSuperfluousNodesFromFifoFolder);
-    }
+    moveBookmarkToTop(id, removeSuperfluousNodesFromFifoFolder);
+  }
 
 
-    chromeBookmarkService.onCreated.addListener(createdHandler);
-    chromeBookmarkService.onMoved.addListener(movedHandler);
+  chromeBookmarkService.onCreated.addListener(createdHandler);
+  chromeBookmarkService.onMoved.addListener(movedHandler);
 
 }
 
 
 chrome.bookmarks.search({title: '!FIFO!'}, function (results) {
-    if (!results.length) {
-        return;
-    }
-    background(chrome.bookmarks, results[0].id);
+  if (!results.length) {
+    return;
+  }
+  background(chrome.bookmarks, results[0].id);
 
-    chrome.commands.onCommand.addListener(function(command) {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            var tab = tabs[0];
 
-            chrome.bookmarks.create({
-                url: tab.url,
-                title: tab.title,
-                parentId: results[0].id
-            });
-        });
+  chrome.commands.onCommand.addListener(function (command) {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+      var tab = tabs[0];
+
+      chrome.bookmarks.create({
+        url: tab.url,
+        title: tab.title,
+        parentId: results[0].id
+      });
     });
+  });
 });
